550a8bbd2329914952f7d9f82d1ee2c68c976e20
Utsav Dave <utsavdave1997@gmail.com>
2021-05-26 14:24:21 -0400

    Update SwiftFlutterBarcodeScannerPlugin.swift

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -146,19 +146,20 @@ protocol ScanBarcodeDelegate {
 }
 
 class BarcodeScannerViewController: UIViewController {
-    private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
-                                      AVMetadataObject.ObjectType.code39,
-                                      AVMetadataObject.ObjectType.code39Mod43,
-                                      AVMetadataObject.ObjectType.code93,
-                                      AVMetadataObject.ObjectType.code128,
-                                      AVMetadataObject.ObjectType.ean8,
-                                      AVMetadataObject.ObjectType.ean13,
-                                      AVMetadataObject.ObjectType.aztec,
-                                      AVMetadataObject.ObjectType.pdf417,
-                                      AVMetadataObject.ObjectType.itf14,
-                                      AVMetadataObject.ObjectType.dataMatrix,
-                                      AVMetadataObject.ObjectType.interleaved2of5,
-                                      AVMetadataObject.ObjectType.qr]
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.code128]
+//     private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+//                                       AVMetadataObject.ObjectType.code39,
+//                                       AVMetadataObject.ObjectType.code39Mod43,
+//                                       AVMetadataObject.ObjectType.code93,
+//                                       AVMetadataObject.ObjectType.code128,
+//                                       AVMetadataObject.ObjectType.ean8,
+//                                       AVMetadataObject.ObjectType.ean13,
+//                                       AVMetadataObject.ObjectType.aztec,
+//                                       AVMetadataObject.ObjectType.pdf417,
+//                                       AVMetadataObject.ObjectType.itf14,
+//                                       AVMetadataObject.ObjectType.dataMatrix,
+//                                       AVMetadataObject.ObjectType.interleaved2of5,
+//                                       AVMetadataObject.ObjectType.qr]
     public var delegate: ScanBarcodeDelegate? = nil
     private var captureSession = AVCaptureSession()
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
